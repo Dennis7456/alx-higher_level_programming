ce908a016eb42673263daec337b3481cac9f8bec
Dennis7456 <denniskiplangat.dk@gmail.com>
2021-11-22 17:39:48 +0300

    Task 2

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-	def replace_in_list(my_list, idx, element):
-		if idx < 0 or idx >= len(my_list):
-			return my_list
-		my_list[idx]=element
+def replace_in_list(my_list, idx, element):
+	if idx < 0 or idx >= len(my_list):
 		return my_list
-#print(replace_in_list([1,2,3,4,5], 3, 10))
+	my_list[idx]=element
+	return my_list
