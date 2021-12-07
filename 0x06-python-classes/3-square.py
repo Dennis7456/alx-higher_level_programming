dbabf1c89396ec9545e1c7f59947c972f78b1279
Dennis7456 <denniskiplangat.dk@gmail.com>
2021-12-07 16:29:28 +0300

    Task 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Documentation for a square class"""
+
+
+class Square():
+    """Square class for a quadrilateral with four equal sides"""
+
+    def __init__(self, size=0):
+        """Sets the initial size value upon instantiation of the class"
+           Throws errors if the value passed in is not an integer
+        Args:
+            size (int, optional): the size of the square object
+        Raises:
+            TypeError: When the value passed in is not an integer
+            ValueError: When the value passed in is less than 0
+        """
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """Returns the area of the square
+        Returns:
+            The area of the square
+        """
+        return self.__size ** 2
