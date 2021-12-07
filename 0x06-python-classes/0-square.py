34b8821546b3a034317d7f911700b4ed1580073c
Dennis7456 <denniskiplangat.dk@gmail.com>
2021-12-07 16:18:10 +0300

    Task 0

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
+"""Documentation for a square class"""
+
+
 class Square():
-	pass
+    """Square class of a quadrilateral with four equal sides
+       Attributes:
+           None
+    """
+    pass
