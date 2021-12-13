3310d90032d862413c6be4b4403f65fda663799f
Dennis7456 <denniskiplangat.dk@gmail.com>
2021-12-14 02:09:36 +0300

    Task 1

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""
+A rectangle.
+"""
+
+
+class Rectangle:
+    """
+    Rectangle functions and data
+    """
+    def __init__(self, width=0, height=0):
+        """
+        Instatiation
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """
+        width getter
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """
+        width setter
+        """
+        if type(value) != int:
+            raise ValueError("Width must be an integer")
+        if (value < 0):
+            raise ValueError("Integer cannot be less than zero")
+    @property
+    def height(self):
+        """
+        height getter
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """
+        height setter
+        """
+        if type(value) != int:
+            raise ValueError("Height must be an integer")
+        if (value < 0):
+            raise ValueError("Integer cannot be less than zero")
+        
