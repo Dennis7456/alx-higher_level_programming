255e54490a1d7eb12344fd286bd677a47296bda8
Dennis7456 <denniskiplangat.dk@gmail.com>
2021-12-21 03:23:00 +0300

    All other tasks

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""
+Contains the inherits_from function
+"""
+
+
 def inherits_from(obj, a_class):
-    if (type(obj) != a_class):
-        return issubclass(type(obj), a_class)
-    return False
+    """returns true if obj is a subclass of a_class, otherwise false"""
+    return(issubclass(type(obj), a_class) and type(obj) != a_class)
