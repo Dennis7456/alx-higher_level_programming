51c598954236d3cd428626cadda9ea29e26f21bb
Dennis Kiplang'at <denniskiplangat.dk@gmail.com>
2021-12-22 12:47:11 +0300

    Task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,17 @@
 #!/usr/bin/python3
+"""
+Read file function
+"""
 
-def read_file(file_name=""):
-    with open(file_name, "r", encoding="UTF-8") as f:
-        print(f.read(), end="")
+
+def read_file(filename=""):
+    """read a file
+    args:
+        filename: file to manipulate
+    return:
+        na
+    """
+
+    with open(filename, encoding="utf-8") as f:
+        for line in f.read():
+            print(line, end="")
