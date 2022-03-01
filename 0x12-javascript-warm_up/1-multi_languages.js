045ed5abc8e1e737bd52ccba03cf3a70b158d590
Dennis Kiplang'at <denniskiplangat.dk@gmail.com>
2022-03-01 13:18:53 +0300

    Task 1

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+const firstString = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let i = 0;
+for (i; i < firstString.length; i++) {
+  console.log(firstString[i]);
+}
