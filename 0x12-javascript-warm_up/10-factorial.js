2c4276cec3c2aea4b432c191da0d8d082cd6a2c0
Dennis Kiplang'at <denniskiplangat.dk@gmail.com>
2022-03-01 15:09:45 +0300

    other tasks

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+// script that computes and prints a factorial
+
+function factorial (n) {
+  if ((isNaN(n)) || (n === 1)) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
+}
+
+console.log(factorial(parseInt(process.argv[2])));
