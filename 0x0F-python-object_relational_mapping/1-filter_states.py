a9aad860ed935cfe94d6beb46be4eaaf087ef6fa
Dennis Kiplang'at <denniskiplangat.dk@gmail.com>
2022-03-05 20:01:38 +0300

    small changes

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 
-"""This script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa"""
+"""This script that lists all states with a name starting with N (upper N)
+from the database hbtn_0e_0_usa"""
 
 import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306,
-    user=argv[1], passwd=argv[2], db=argv[3])
-  cur = db.cursor()
+                         user=argv[1], passwd=argv[2], db=argv[3])
+    cur = db.cursor()
     sql = "SELECT * FROM states WHERE name='{:s}'\
     ORDER BY states.id".format(argv[4])
     cur.execute(sql)
@@ -18,4 +19,3 @@ if __name__ == "__main__":
             print(row)
     cur.close()
     db.close()
-
