58d29569d8a8ecc74536dbdc30c29f8a5fa61759
Dennis Kiplang'at <denniskiplangat.dk@gmail.com>
2022-03-05 19:59:30 +0300

    small changes

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,9 +6,9 @@ import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-	db = MySQLdb.connect(host="localhost", port=3306,
-		user=argv[1], passwd=argv[2], db=argv[3])
-	cur = db.cursor()
-	num_rows = cur.execute("SELECT * FROM states ORDER BY states.id")
-	for i in range(num_rows):
-		print(cur.fetchone())
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    num_rows = cur.execute("SELECT * FROM states ORDER BY states.id")
+    for i in range(num_rows):
+        print(cur.fetchone())
---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""This script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306,
+    user=argv[1], passwd=argv[2], db=argv[3])
+  cur = db.cursor()
+    sql = "SELECT * FROM states WHERE name='{:s}'\
+    ORDER BY states.id".format(argv[4])
+    cur.execute(sql)
+    rows = cur.fetchall()
+    for row in rows:
+        if row[1] == argv[4]:
+            print(row)
+    cur.close()
+    db.close()
+
