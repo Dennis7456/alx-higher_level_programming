b90b6666b1b2b1427ebd095bf101bc09ca7b402e
Dennis7456 <denniskiplangat.dk@gmail.com>
2022-03-23 01:14:14 +0300

    Task 6

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -3,7 +3,7 @@
 Python script that takes in a URL, sends a request to the URL and displays
 the value of the variable X-Request-Id in the response header
 """
-if __name__ == "__main__":
+if __name__== "__main__":
     import requests
     from sys import argv
     r = requests.get(argv[1])
--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -4,9 +4,6 @@ Python script that takes in a URL and an email address, sends a POST request
 to the passed URL with the email as a parameter, and finally displays the
 body of the response.
 """
-from ast import arg
-
-
 if __name__ == "__main__":
     import requests
     from sys import argv
