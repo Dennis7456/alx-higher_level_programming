bb90266e583e7ba6f849d42a5f98396972413794
Dennis7456 <denniskiplangat.dk@gmail.com>
2022-03-23 01:28:18 +0300

    Task 7

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a URL, sends a request to the URL and displays
+the body of the response
+"""
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+    r = requests.get(argv[1])
+    if r.status_code >= 400:
+        print("Error code: {}".format(r.status_code))
+    else:
+        print(r.text)
