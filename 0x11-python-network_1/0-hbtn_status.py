d4158457580275a5c2d46e3102755439848610d4
Dennis7456 <denniskiplangat.dk@gmail.com>
2022-03-22 23:58:08 +0300

    small changes

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,14 +2,13 @@
 
 """
 Python script that fetches https://alx-intranet.hbtn.io/status
-using urllib 
+using urllib
 """
 if __name__ == "__main__":
-	import urllib.request as request
-	with request.urlopen("https://alx-intranet.hbtn.io/status") as r:
-		html = r.read()
-		print('Body response:')
-		print('\t- type: {}'.format(type(html)))
-		print('\t- content: {}'.format(html))
-		print('\t- utf-8 content: {}'.format(html.decode('utf-8')))
-
+    import urllib.request as request
+    with request.urlopen("https://alx-intranet.hbtn.io/status") as r:
+        html = r.read()
+        print('Body response:')
+        print('\t- type: {}'.format(type(html)))
+        print('\t- content: {}'.format(html))
+        print('\t- utf-8 content: {}'.format(html.decode('utf-8')))
