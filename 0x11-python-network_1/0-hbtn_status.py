fa3bbbe36e0a9c69fed9e79b1d4c375ab5a72481
Dennis7456 <denniskiplangat.dk@gmail.com>
2022-03-22 23:42:41 +0300

    Task 0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""
+Python script that fetches https://alx-intranet.hbtn.io/status
+using urllib 
+"""
+if __name__ == "__main__":
+	import urllib.request as request
+	with request.urlopen("https://alx-intranet.hbtn.io/status") as r:
+		html = r.read()
+		print('Body response:')
+		print('\t- type: {}'.format(type(html)))
+		print('\t- content: {}'.format(html))
+		print('\t- utf-8 content'.format(html.decode('utf-8')))
+
