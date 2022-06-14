a82d7f77d67480a5c0afb81b59bfc75fc5197310
Michael Ghalagher <lively222@live.com>
2022-06-14 20:40:31 +0300

    Task 01

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
+
 import random
+
+'''This program will assign a random signed
+number to the variable number each time it
+is executed. Complete the source code
+in order to print the last digit of
+the number stored in
+the variable number.'''
+
 number = random.randint(-10000, 10000)
 last_digit = int(repr(number)[-1])
 if last_digit > 5:
